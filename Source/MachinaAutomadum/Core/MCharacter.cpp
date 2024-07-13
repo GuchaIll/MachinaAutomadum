0173220d97eb74e064b56cfae97dabe98b5573b4
GuchaIll <guchaillustrate@gmail.com>
2024-07-13 13:46:21 -0500

    interact added

---
+++ Source/MachinaAutomadum/Core/MCharacter.cpp
@@ -0,0 +1,156 @@
+#include "MCharacter.h"
+#include "GameplayTagContainer.h"
+#include "../AbilitySystem/Attributes/MAttributeSet.h"
+#include "../AbilitySystem/MAbilitySystemComponent.h"
+#include "../AbilitySystem/Abilities/MGameplayAbility.h"
+#include "kismet/GameplayStatics.h"
+
+AMCharacter::AMCharacter()
+{
+
+    PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UMAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetHealthAttribute()).AddUObject(this, &AMCharacter::OnHealthChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetMaxHealthAttribute()).AddUObject(this, &AMCharacter::OnMaxHealthChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetEnergyAttribute()).AddUObject(this, &AMCharacter::OnEnergyChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetMaxEnergyAttribute()).AddUObject(this, &AMCharacter::OnMaxEnergyChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetArmorAttribute()).AddUObject(this, &AMCharacter::OnArmorChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetMaxArmorAttribute()).AddUObject(this, &AMCharacter::OnMaxArmorChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetLevelAttribute()).AddUObject(this, &AMCharacter::OnLevelChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetShieldAttribute()).AddUObject(this, &AMCharacter::OnShieldChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetMaxShieldAttribute()).AddUObject(this, &AMCharacter::OnMaxShieldChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetShieldStackAttribute()).AddUObject(this, &AMCharacter::OnShieldStackChanged);
+    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UMAttributeSet::GetBaseDamageAttribute()).AddUObject(this, &AMCharacter::OnBaseDamageChanged);
+
+    DeathTag = FGameplayTag::RequestGameplayTag(FName("State.Dead"));
+}
+
+UAbilitySystemComponent *AMCharacter::GetAbilitySystemComponent() const
+{
+    return AbilitySystemComponent;
+}
+
+void AMCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+
+    for(TSubclassOf<UMGameplayAbility> Ability : DefaultAbilities)
+	{
+		AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(Ability, 1, 0));
+	}
+
+    for(TSubclassOf<UMGameplayAbility> Ability : CharacterAbilities)
+	{
+		AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(Ability, 1, 0));
+	}
+
+    for(TSubclassOf<UGameplayEffect> Effect : StartupEffects)
+    {
+        FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+        EffectContext.AddSourceObject(this);
+
+        FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(Effect, 1, EffectContext);
+        if(NewHandle.IsValid())
+        {
+            FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*NewHandle.Data.Get());
+        }
+    }
+
+    
+
+
+   
+    
+}
+
+void AMCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+}
+
+
+void AMCharacter::OnHealthChanged(const FOnAttributeChangeData &Data)
+{
+     Health = Data.NewValue;
+
+    if(Health <= 0.0f && !AbilitySystemComponent->HasMatchingGameplayTag(DeathTag))
+	{
+		//Perform Death Related Actions
+		AddTag(DeathTag);
+
+		OnDeath();
+	}
+
+}
+
+void AMCharacter::OnMaxHealthChanged(const FOnAttributeChangeData &Data)
+{
+    MaxHealth = Data.NewValue;
+
+}
+
+void AMCharacter::OnEnergyChanged(const FOnAttributeChangeData &Data)
+{
+    Energy = Data.NewValue;
+}
+
+void AMCharacter::OnMaxEnergyChanged(const FOnAttributeChangeData &Data)
+{
+    MaxEnergy = Data.NewValue;
+}
+
+void AMCharacter::OnArmorChanged(const FOnAttributeChangeData &Data)
+{
+    Armor = Data.NewValue;
+}
+
+void AMCharacter::OnMaxArmorChanged(const FOnAttributeChangeData &Data)
+{   
+    MaxArmor = Data.NewValue;
+}
+
+void AMCharacter::OnLevelChanged(const FOnAttributeChangeData &Data)
+{
+    Level = Data.NewValue;
+
+}
+
+void AMCharacter::OnShieldChanged(const FOnAttributeChangeData &Data)
+{
+    Shield = Data.NewValue;
+}
+
+void AMCharacter::OnMaxShieldChanged(const FOnAttributeChangeData &Data)
+{
+    MaxShield = Data.NewValue;
+}
+
+void AMCharacter::OnShieldStackChanged(const FOnAttributeChangeData &Data)
+{
+    ShieldStack = Data.NewValue;
+}
+
+void AMCharacter::OnBaseDamageChanged(const FOnAttributeChangeData &Data)
+{
+    BaseDamage = Data.NewValue;
+}
+
+void AMCharacter::OnDeath()
+{
+    
+}
+
+void AMCharacter::OnDamageTaken(const FOnAttributeChangeData &Data)
+{
+}
+
+void AMCharacter::GetOwnedGameplayTags(FGameplayTagContainer &TagContainer) const
+{
+    TagContainer = OwnedTags;
+}
---
+++ Source/MachinaAutomadum/Core/MCharacter.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "../Renders/DamageTextRenderComponent.h"
+#include "../AbilitySystem/Attributes/MAttributeSet.h"
+#include "GameplayTagContainer.h"
+#include "GameplayEffectTypes.h"
+#include "../AbilitySystem/Abilities/MGameplayAbility.h"
+#include "GameplayTagAssetInterface.h"
+#include "MCharacter.generated.h"
+
+class UMAbilitySystemComponent;
+class UMGameplayAbility;
+class UMAttribute;
+
+UCLASS()
+class MACHINAAUTOMADUM_API AMCharacter : public ACharacter, public IAbilitySystemInterface, public IGameplayTagAssetInterface
+{
+    GENERATED_BODY()
+
+    public:
+
+    AMCharacter();
+
+    UAbilitySystemComponent *GetAbilitySystemComponent() const override;
+
+    virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+
+    UFUNCTION(BlueprintCallable, Category = "Gameplay Tag")
+	void AddTag(const FGameplayTag& Tag)
+    {
+        OwnedTags.AddTag(Tag);
+    } 
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Tag")
+    void RemoveTag(const FGameplayTag& Tag)
+    {
+        OwnedTags.RemoveTag(Tag);
+    }
+
+
+    protected:
+
+    virtual void BeginPlay() override;
+
+    
+    virtual void Tick(float DeltaTime) override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability")
+    UMAbilitySystemComponent *AbilitySystemComponent;
+
+   
+    //Any default effects that should be applied to the character
+    //on spawn, for instance effects carried over from character switching
+    UPROPERTY(BlueprintReadWrite, Category = "Ability")
+    TArray<TSubclassOf<class UGameplayEffect>> StartupEffects;
+
+    //Array of abilities that the character can use
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability")
+    TArray<TSubclassOf<class UMGameplayAbility>> CharacterAbilities;
+
+    //standard sets of abilities that all characters share (for instance death)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability")
+    TArray<TSubclassOf<class UMGameplayAbility>> DefaultAbilities;
+
+    float Health, MaxHealth, Energy, MaxEnergy, Armor, MaxArmor, Level, Shield, MaxShield, ShieldStack, BaseDamage;
+
+    //Series of delegate functions for updating states
+    virtual void OnHealthChanged(const FOnAttributeChangeData& Data);
+
+	virtual void OnMaxHealthChanged(const FOnAttributeChangeData& Data);
+
+	virtual void OnEnergyChanged(const FOnAttributeChangeData& Data);
+
+	virtual void OnMaxEnergyChanged(const FOnAttributeChangeData& Data);
+
+	virtual void OnArmorChanged(const FOnAttributeChangeData& Data);
+
+	virtual void OnMaxArmorChanged(const FOnAttributeChangeData& Data);
+
+	virtual void OnLevelChanged(const FOnAttributeChangeData& Data);
+
+    virtual void OnShieldChanged(const FOnAttributeChangeData& Data);
+
+    virtual void OnMaxShieldChanged(const FOnAttributeChangeData& Data);
+
+    virtual void OnShieldStackChanged(const FOnAttributeChangeData& Data);
+
+    virtual void OnBaseDamageChanged(const FOnAttributeChangeData& Data);
+
+    virtual void OnDeath();
+
+    virtual void OnDamageTaken(const FOnAttributeChangeData& Data);
+
+    //Tags owned by the character
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	FGameplayTagContainer OwnedTags;
+
+    FGameplayTag DeathTag;
+};
